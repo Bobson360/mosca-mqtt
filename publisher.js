844de6d8fea27f7afe770d1b0c00147eced292f7
Robson Rodrigues <bobson278@gmail.com>
2019-05-03 00:16:22 -0300

    make it better

--- publisher.js
+++ publisher.js
@@ -1,8 +1,8 @@
 var mqtt = require('mqtt');
-var client  = mqtt.connect('mqtt://your_ip_address_of_pc');
+var client  = mqtt.connect('mqtt://192.168.15.13');
 client.on('connect', function () {
 setInterval(function() {
-client.publish('myTopic', 'Hello mqtt');
+client.publish('myTopic', 'Testando mqtt e arduino');
 console.log('Message Sent');
 }, 5000);
 });
--- subscriber.js
+++ subscriber.js
@@ -1,9 +1,21 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://your_ip_address_of_pc')
+var client  = mqtt.connect('mqtt://192.168.15.13')
 client.on('connect', function () {
-    client.subscribe('myTopic')
+    client.subscribe('myTopic2')
 })
 client.on('message', function (topic, message) {
 context = message.toString();
 console.log(context)
+if(context == 'arduino020519'){
+    msg1()
+}else{
+    msg2()
+}
 })
+
+var msg1 = function(){
+    console.log("arduino020519 -> publicando na API")
+}
+var msg2 = function(){
+    console.log("esp8266020519 -> publicando na API")
+}
